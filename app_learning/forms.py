c587bf39b71e83ff559ddbf9006effd4ffc0b779
mateusfl1998 <154616064+mateusfl1998@users.noreply.github.com>
2024-03-31 14:58:20 -0300

    form de login e cadastro funcionando

--- app_learning/forms.py
+++ app_learning/forms.py
@@ -11,3 +11,8 @@ class EntryForm(forms.ModelForm):
         model = Entry
         fields = ['text']
         widgets = {'text': forms.Textarea(attrs={'cols':80})}
+    
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['user'] = self.user_id
+        return initial
