fda2353b19e6b2502439977249bfe076e4235720
mateusfl1998 <154616064+mateusfl1998@users.noreply.github.com>
2024-04-04 12:44:55 -0300

    readme

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request,user)
-            return redirect('topics_list')
+            return redirect(to='topics_list')
         else:
             login_form = AuthenticationForm() 
     else:
